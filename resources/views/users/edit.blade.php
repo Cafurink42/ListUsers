d888d8fe9559211106dabe5688b1cfd6c93a841f
Cafurink42 <carlosdanielcotticaguth@gmail.com>
2025-10-23 23:37:50 -0300

    Criação da rota edit e view para edição do usuário

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Usuário</title>
+</head>
+<body>
+
+    <div class="container">
+        <h1>Edit User</h1>
+
+        <form method="POST" action="">
+           
+        </form>
+    </div>
+    
+</body>
+</html>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,12 @@ Route::post('/users/store', [UserController::class, 'store'])->name('users.store
 
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 
-Route::get('/users/list',[UserController::class, 'list'])->name('users.index');
+Route::get('/users/list',[UserController::class, 'list'])->name('users.index'); //list
 
-Route::delete('/users/{id}', [UserController::class,'destroy'])->name('users.destroy');
+Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit')->whereNumber('id');
+
+Route::put('/users/edit/{id}', [UserController::class, 'update'])->name('users.update')->whereNumber('id');
+
+Route::delete('/users/{id}', [UserController::class,'destroy'])->whereNumber('id')->name('users.destroy');
+
+//Route::update('/users/edit',[UserController::class,'update'])->name('users.update');
